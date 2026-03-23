6980fbf4d78a41a29624e9809954a38d53199ee5
Aviram Hassan <aviram@metalbear.com>
2026-03-23 22:55:15 +0200

    remove terraform

--- deploy/terraform/registry.tf
+++
@@ -1,6 +0,0 @@
-resource "google_artifact_registry_repository" "sandcastle" {
-  location      = var.region
-  repository_id = "sandcastle"
-  format        = "DOCKER"
-  description   = "Sandcastle container images"
-}
